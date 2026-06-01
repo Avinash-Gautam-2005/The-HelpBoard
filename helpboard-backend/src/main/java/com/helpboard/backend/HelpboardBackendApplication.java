fc7ee93eac4906269c90ea698011c36fec01237d
Avi Gautam <avigautam2005@gmail.com>
2026-06-01 17:11:01 +0530

    Remove explicit EnableJpaRepositories annotation to use Spring Boot auto-config

--- helpboard-backend/src/main/java/com/helpboard/backend/HelpboardBackendApplication.java
+++ helpboard-backend/src/main/java/com/helpboard/backend/HelpboardBackendApplication.java
@@ -4,12 +4,10 @@ import org.modelmapper.ModelMapper; // Import for ModelMapper
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.helpboard.backend.repository")
 public class HelpboardBackendApplication {
 
     public static void main(String[] args) {
