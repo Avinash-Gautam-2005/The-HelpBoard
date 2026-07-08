ef47b74af045c1be107716b1001487d56f9772b1
Avi Gautam <avigautam2005@gmail.com>
2026-07-09 00:55:47 +0530

    Allow all origin patterns for WebSockets to resolve CORS block in production

--- helpboard-backend/src/main/java/com/helpboard/backend/config/WebSocketConfig.java
+++ helpboard-backend/src/main/java/com/helpboard/backend/config/WebSocketConfig.java
@@ -42,7 +42,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-        .setAllowedOriginPatterns("http://localhost:3000", "http://127.0.0.1:3000")
+        .setAllowedOriginPatterns("http://localhost:3000", "http://127.0.0.1:3000", "*")
         .withSockJS();
     }
 
